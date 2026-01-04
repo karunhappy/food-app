a382bed1650f99fb83346614acbce25132e3ac40
karunhappy <karun.happy358@gmail.com>
2026-01-04 18:15:23 +0530

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Karun Aggarwal on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
